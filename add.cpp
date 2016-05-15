6bcd6b254c6f62e9f27ba2f0b71200365089d080
akshaygore06 <agore1@mail.csuchico.edu>
2016-05-14 18:21:03 -0700

    addition of 2 and 3

---
+++ add.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <assert.h>
+
+using namespace std;
+
+int main()
+{
+	int x = 2;
+	int y = 3;
+	int z = 0;
+
+	z = x + y;
+
+	assert(z == 5);
+	cout << "value of z is 5"<<endl;
+
+	return 0;
+}
